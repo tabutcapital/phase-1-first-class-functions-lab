3a0eed99e7120c6840df62fa1cba0079ef8dd357
tabutcapital <iantabut3@gmail.com>
2024-10-23 15:38:38 +0300

    advancing functions

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers) {
+    return [...drivers].slice(null,2)
+}
+const returnLastTwoDrivers = function(drivers) {
+    return [...drivers].splice(2)
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+console.log(selectingDrivers);
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return multiplier*fare
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, selectionfunction) {
+    return selectionfunction(drivers)
+}
+
+
+  
